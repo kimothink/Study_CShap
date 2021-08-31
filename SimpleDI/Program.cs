8cc36beee8b4c055585ef8f854ee6042902ad34b
DESKTOP-30RJMTC\kimo8 <kimo87@gmail.com>
2021-09-01 02:18:19 +0900

    초기 커밋.

---
+++ SimpleDI/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace SimpleDI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILogger logger;
+
+            string loggerType = "database";
+
+            switch(loggerType)
+            {
+                case "database":
+                    logger = new DatabaseLogger();
+                    break;
+                default:
+                    logger = new TextLogger();
+                    break;
+
+            }
+
+            LoggerManager loggerManager = new LoggerManager(logger);
+
+            try
+            {
+                throw new DivideByZeroException();
+            }
+            catch(Exception e)
+            {
+                loggerManager.Log(e.Message);
+                Console.ReadLine();
+            }
+         
+        }
+    }
+
+    interface ILogger
+    {
+        void Log(string message);
+    }
+
+    class LoggerManager
+    {
+        private ILogger _logger;
+
+        public LoggerManager(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Log(string message)
+        {
+            _logger.Log(message);
+        }
+    }
+
+    class TextLogger :ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine("Log to a text file: " + message);
+        }
+    }
+    class DatabaseLogger : ILogger
+    {
+        public void Log(string messaage)
+        {
+            Console.WriteLine("Log to a database :" + messaage);
+        }
+    }
+}
